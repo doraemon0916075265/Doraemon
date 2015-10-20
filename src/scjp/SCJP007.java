228e75a5fe1c5b6be40632ffe8a65ab1c54bef49
user <user@doraemon>
2015-10-21 00:16:45 +0800

    SCJP10

---
+++ src/scjp/SCJP007.java
@@ -0,0 +1,12 @@
+package scjp;
+
+public class SCJP007 {
+
+	public static void main(String[] args) {
+		int b = 4;
+		b--;
+		System.out.println(--b);
+		System.out.println(b);
+	}
+
+}
---
+++ src/scjp/SCJP008SampleClosable.java
@@ -0,0 +1,5 @@
+package scjp;
+
+public interface SCJP008SampleClosable {
+	public void close() throws java.io.IOException;
+}
